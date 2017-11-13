0cf843b9db7ecd89645adebb2c14b3f9a077feb1
HeruSantoso <herusantoso008@gmail.com>
2017-11-13 13:40:10 +0700

    add fragment mata kuliah

--- app/src/main/java/com/sinau/simikmiriskina/Home.java
+++ app/src/main/java/com/sinau/simikmiriskina/Home.java
@@ -1,9 +1,9 @@
 package com.sinau.simikmiriskina;
 
+
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -71,6 +71,7 @@ public class Home extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        FragmentManager fragmentManager = getFragmentManager();
 
         if (id == R.id.nav_profil) {
             // Handle the camera action
@@ -78,8 +79,10 @@ public class Home extends AppCompatActivity
 
         } else if (id == R.id.nav_ambilKelas) {
 
-
-
+        } else if (id == R.id.nav_mata_kulish) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, new MataKuliahFragment())
+                    .commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
